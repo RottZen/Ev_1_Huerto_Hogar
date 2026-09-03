a9d34a90a5edae7dca1228cecb43be533b7b926b
SJ- Alumno <SJ-Alumno@duoc.acad>
2026-09-03 18:14:39 -0400

    cambio js

---
+++ js/catalogo.js
@@ -0,0 +1,37 @@
+const LLAVE = "Prueba_1"
+productos = [
+    {
+        "Codigo": "FR001",
+        "Nombre": "Manzanas Fuji",
+        "Precio": 1200,
+        "Stock": 150
+    },
+
+    {
+        "Codigo": "FR002",
+        "Nombre": "Naranjas Valencia",
+        "Precio": 1000,
+        "Stock": 200
+    },
+
+    {
+        "Codigo": "FR003",
+        "Nombre": "Plátanos Cavendish",
+        "Precio": 800,
+        "Stock": 250
+    },
+
+    {
+        "Codigo": "VR001",
+        "Nombre": "Zanahorias Organicas",
+        "Precio": 900,
+        "Stock": 100
+    },
+
+    {
+        "Codigo": "VR002",
+        "Nombre": "Espinacas Frescas",
+        "Precio": 700,
+        "Stock": 80
+    },
+]
